59b4020a4731b517a92ba5d154369dfe2dc24d29
semn43 <osincev.semn@mail.ru>
2021-08-14 23:52:40 +0500

    Js has been prisutstvuet

---
+++ Js/mecha.js
@@ -0,0 +1,16 @@
+
+//Включение строгого режима
+"use sctrict"
+//Включение строгого режима
+
+function showMessage() {
+	const el = document.getElementById("el");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
+
+function showMessageA() {
+	const el = document.getElementById("block_twos");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
+
+
