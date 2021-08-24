8da884e3c5c7599682aacb8d1f8f48529a140531
semn43 <osincev.semn@mail.ru>
2021-08-25 01:43:18 +0500

    Animation +-already

--- Js/mecha.js
+++ Js/mecha.js
@@ -3,14 +3,57 @@
 "use sctrict"
 //Включение строгого режима
 
-function showMessage() {
-	const el = document.getElementById("el");
+
+function showMessageH() {
+	const el = document.getElementById("block_one");
 	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
 }
+
 
 function showMessageA() {
-	const el = document.getElementById("block_twos");
+const el= document.getElementById("block_twos");
 	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
 }
 
+function showMessageE() {
+const el= document.getElementById("block_four");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
 
+function showMessageT() {
+const el= document.getElementById("block_five");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
+/*Пока что этих блоков нет
+function showMessageW() {
+const el= document.getElementById("block_twos");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
+
+function showMessageP() {
+const el= document.getElementById("block_twos");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
+
+function showMessageC() {
+const el= document.getElementById("block_twos");
+	el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
+}
+Пока что этих блоков нет*/
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lendnig-page
+https://codepen.io/pelegrin2puk/pen/vYXvjpd - Код для появления сайта при скролле
